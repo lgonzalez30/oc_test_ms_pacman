672a8ac6f895d2b5e7c0bbfcd42c4273436075bc
Luis Gonzalez <luisgonzalez@MacBook-Pro-de-Luis.local>
2024-09-07 11:04:34 -0500

    hice un cambio donde agregue el servicio de tasks

--- app.py
+++ app.py
@@ -16,7 +16,14 @@ def alive():
     return  jsonify({'message': 'OK', 'status': True}), 200
 
 
+@app.route('/v1/tasks', methods=['GET'])
+def tasks():
+    return jsonify([{'nombre': 'task 1', 'hora': '10:30:00','done': True}, {'nombre': 'task 2', 'hora': '10:40:00', 'done':False}]), 200
 
 port = int(os.environ.get('PORT', 5001))
 if __name__ == '__main__':
     app.run(debug=True, host='0.0.0.0', port=port)
+    
+    
+    
+
